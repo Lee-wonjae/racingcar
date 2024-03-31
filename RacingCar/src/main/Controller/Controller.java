26ad4b17cc5ac5e513d0530174e03ccf62c46249
jerry <lwj0173@gmail.com>
2024-03-31 20:08:19 +0900

    controller mod

--- RacingCar/src/main/Controller/Controller.java
+++ RacingCar/src/main/Controller/Controller.java
@@ -1,6 +1,7 @@
 package main.Controller;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.function.Supplier;
 import main.Domain.Car;
 import main.Domain.*;
@@ -9,46 +10,19 @@ import main.View.*;
 
 public class Controller {
 
+    private final InputValidator inputValidator;
+    private final InputParser inputParser;
+    private final Scanner scanner;
     private final Input input;
     private final Output output;
 
-    public Controller(final Input input, final Output output) {
+    public Controller(InputValidator inputValidator, InputParser inputParser, Scanner scanner, final Input input, final Output output) {
+        this.inputValidator = inputValidator;
+        this.inputParser = inputParser;
+        this.scanner = scanner;
         this.input = input;
         this.output = output;
     }
 
-    public <T> T retry(final Supplier<T> supplier) {
-        try {
-            return supplier.get();
-        } catch (IllegalArgumentException e) {
-            output.printErrorMessage(e.getMessage());
-            return retry(supplier);
-        }
-    }
 
-    public void run() {
-        CarsGame Game =new CarsGame(input.readCarNames(),input.readCount());
-        play(racingGame);
-        findWinners(racingGame);
-    }
-
-    private CarsGame initialize() {
-        List<String> carNames = retry(inputView::readCarNames);
-        int count = retry(inputView::readCount);
-        return new RacingGame(new RandomNumberGenerator(), carNames, count);
-    }
-
-    private void play(final RacingGame racingGame) {
-        outputView.printResultMessage();
-        while (racingGame.isPlayable()) {
-            racingGame.play();
-            List<Car> cars = racingGame.findCurrentCarPositions();
-            outputView.printCurrentCarPositions(cars);
-        }
-    }
-
-    private void findWinners(final RacingGame racingGame) {
-        List<String> winners = racingGame.findWinners();
-        outputView.printWinnersMessage(winners);
-    }
 }
--- RacingCar/src/main/Domain/SettingCars.java
+++
@@ -1,8 +0,0 @@
-package main.Domain;
-
-import java.util.List;
-import main.View.*;
-public class SettingCars {
-    private Input input;
-    private List<String> carsName= new input.readCarNames();
-}
