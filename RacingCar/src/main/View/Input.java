dab70f3d16368f0ddd35bf0e772a15b81b50158a
jerry <lwj0173@gmail.com>
2024-03-31 14:34:27 +0900

    output modi

--- RacingCar/src/main/View/Input.java
+++ RacingCar/src/main/View/Input.java
@@ -8,11 +8,11 @@ public class Input {
     private static final String READ_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String READ_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    private final racingcar.view.InputValidator inputValidator;
-    private final racingcar.view.InputParser inputParser;
+    private final main.View.InputValidator inputValidator;
+    private final main.View.InputParser inputParser;
     private final Scanner scanner;
 
-    public Input(final racingcar.view.InputValidator inputValidator, final racingcar.view.InputParser inputParser, final Scanner scanner) {
+    public Input(final main.View.InputValidator inputValidator, final main.View.InputParser inputParser, final Scanner scanner) {
         this.inputValidator = inputValidator;
         this.inputParser = inputParser;
         this.scanner = scanner;
